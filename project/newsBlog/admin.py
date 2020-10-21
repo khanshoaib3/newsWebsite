225dac8a7a6a56e3f88d34b98573e58e83d9e1b8
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-10-21 10:52:57 -0700

    *Added search filter in django admin *Added filter field in django admin

--- project/newsBlog/admin.py
+++ project/newsBlog/admin.py
@@ -4,4 +4,10 @@ from .models import Post
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-	list_display=['title','author','updated','status']
+	list_display=('title','author','updated','publish','status')
+	list_filter=('author','publish','status','created')
+	search_fields=('title','body')
+	prepopulated_fields={'slug':('title',)}
+	raw_id_fields = ('author',)
+	date_hierarchy = 'publish'
+	ordering = ('status', 'publish')
