6d6b90e42444994596ecfa279e6183133f520862
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-10-21 11:37:45 -0700

    *Added unique id to posts

--- project/newsBlog/models.py
+++ project/newsBlog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+import uuid
 
 
 #Custom model manager to filter posts according to published status
@@ -12,6 +13,7 @@ class PublishedManager(models.Manager):
 
 # Create your models here.
 class Post(models.Model):
+	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
 		('published', 'Published'),
