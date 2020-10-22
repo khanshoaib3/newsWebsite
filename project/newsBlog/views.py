f7a639f7b6cc8897aa0016afff96397e1ac205c9
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-10-22 13:59:02 +0530

    *Adding filter flip animation

--- project/newsBlog/views.py
+++ project/newsBlog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def list(request):
-	return render(request,'newsBlog/files/listBlog.html',{'nav':'common/nav.html'})
+	return render(request,'newsBlog/files/listBlog.html',{'nav':'common/nav.html','commonCss':'newsBlog/files/commonCss.html','posts':Post.published.all(),'css':'newsBlog/files/listBlogCss.html'})
