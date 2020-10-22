2a8bb42da2fd9905706428c0fad1bcc164c57b8e
shoaib11120 <mdshoaibkhan.1999@gmail.com>
2020-10-21 23:37:17 -0700

    *Added listblog.html file to the url

--- project/newsBlog/urls.py
+++ project/newsBlog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
+from .views import list
 
 app_name = 'newsBlog'
 
 urlpatterns = [
-	
+	path('' ,list ,name = 'list'),
 ]
--- project/newsBlog/views.py
+++ project/newsBlog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list(request):
+	return render(request,'newsBlog/files/listBlog.html',{'nav':'common/nav.html'})
