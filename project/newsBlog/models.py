9dece4355cb9f3b37d46f31a8b5ed0d46c0bd8c1
Shaoib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-04 14:28:25 +0530

    *Added comment model in newsBlog and migrated

--- project/newsBlog/models.py
+++ project/newsBlog/models.py
@@ -38,3 +38,18 @@ class Post(models.Model):
 		 return reverse('newsBlog:detail',args=[self.publish.year,
 		 										self.publish.month,
 												self.publish.day, self.slug])
+
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+	name = models.CharField(max_length=50)
+	username = models.CharField(max_length=150)
+	body = models.TextField(blank=False)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	active = models.BooleanField(default=True)
+	class Meta:
+		ordering = ()
+	def __str__(self):
+		return f'Comment by {self.name} on {self.post}'
+    
