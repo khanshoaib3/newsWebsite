55b386a070c043e26cf271b67b65040cbbe31dfa
Shaoib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-05 17:30:59 +0530

    *Fixing static bug

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.homeView),
     path('admin/', admin.site.urls),
     path('account/',include('account.urls','account')),
     path('news/',include('newsBlog.urls','news')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
