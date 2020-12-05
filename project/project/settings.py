55a1b7626fc75cc4c0ddeeba7ec614261f3d3213
Shaoib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-05 17:34:42 +0530

    *cgnvbm

--- project/project/settings.py
+++ project/project/settings.py
@@ -133,4 +133,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
