4777010626070c215c33a3f8bf4756f468ddcf50
Shaoib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-05 17:40:26 +0530

    sdfhj

--- project/project/settings.py
+++ project/project/settings.py
@@ -133,5 +133,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
