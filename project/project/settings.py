057cc73f4bc1bb56e43ecff83f1e78579013ec37
Shaoib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-08 14:42:57 +0530

    *Changed debug to false

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = 'y7=b&3=-%^dhm2v$8tkp7fcyzi1f526iru=_g4%c=^&i@4_+o0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','shoaib12120.pythonanywhere.com',]
 
