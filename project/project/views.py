bb50a41aa87172f306305cf2965fc27b753ae700
Shaoib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-11 12:00:26 +0530

    *Added divs on home page using tags

--- project/project/views.py
+++ project/project/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from newsBlog.models import Post
 
 def homeView(request):
     return render(request,
                 'home/files/homeContent.html',
                 {'css':'home/files/homeCss.html',
-                'nav':'common/nav.html'})
+                'nav':'common/nav.html',
+                'globals':Post.published.filter(tag__name__in=["global","globals"]).distinct(),
+                'techs':Post.published.filter(tag__name__in=["tech","technical","technology"]).distinct(),
+                'gamings':Post.published.filter(tag__name__in=["game","gaming","games"]).distinct(),
+                'entertainments':Post.published.filter(tag__name__in=["entertainment"]).distinct()})
