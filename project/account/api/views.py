7736b8162b45505d1b421a4d551a050aca4109dc
Shoaib Khan <mdshoaibkhan.1999@gmail.com>
2020-12-13 23:22:26 +0530

    Get user token on signup through api

--- project/account/api/views.py
+++ project/account/api/views.py
@@ -5,6 +5,8 @@ from rest_framework.parsers import JSONParser
 from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
 
 @api_view(['GET', 'POST'])
 def hello_world(request):
@@ -21,5 +23,7 @@ def createUserView(request):
         serializer = CreateUserSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
+            user = User.objects.get(username=serializer.data['username'])
+            token = Token.objects.get(user=user).key
+            return JsonResponse({'Token':token})
         return JsonResponse(serializer.errors, status=400)
