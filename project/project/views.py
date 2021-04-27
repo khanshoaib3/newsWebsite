d1d63ef2a7d70fec85b63f10e5940c79f0322ab6
Shoaib Khan <mdshoaibkhan.1999@gmail.com>
2021-04-27 18:30:40 +0530

    adding automatic tags

--- project/project/views.py
+++ project/project/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from newsBlog.models import Post
+from taggit.models import Tag
 
 def homeView(request):
+    tags = Tag.objects.all()
+    tagName = ""
+    for tag in tags:
+        tagName = tagName + "," + tag.name
     return render(request,
                 'home/files/homeContent.html',
                 {'css':'home/files/homeCss.html',
                 'nav':'common/nav.html',
-                'discord':Post.published.filter(tags__name__in=["discord"]).distinct(),
-                'minecraft':Post.published.filter(tags__name__in=["minecraft"]).distinct(),
+                'tagnames':tagName,
+                'tags':{Post.published.filter(tags__name__in=["discord"]).distinct(),Post.published.filter(tags__name__in=["minecraft"]).distinct(),},
                 'latest':Post.published.all()[:5]})
